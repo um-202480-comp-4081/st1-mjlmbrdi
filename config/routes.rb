07a1a5104f86fa4902c498ac01c68d12827d14d0
mikelombardi4332 <157412901+mikelombardi4332@users.noreply.github.com>
2024-10-08 14:45:12 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: redirect('home')
+  get 'home', to: 'pages#home', as: 'home'
+end
